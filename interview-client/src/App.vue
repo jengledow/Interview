2fe28a2645bc35cf207675abb0ec875a66f9ca8a
jengledow <jmengledow5@gmail.com>
2025-04-04 21:44:55 -0400

    initialized vue project with configs and everything

---
+++ interview-client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <a href="/">Home</a>
+    <a href="/not-home">Not home</a>
+  </header>
+  <RouterView />
+</template>
